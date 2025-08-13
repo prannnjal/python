6f03fd492ff6dd1a73e5a694b99b5287402e093e
prannnjal_ <pranjalkumar445@gmail.com>
2025-08-14 01:09:54 +0530

    set 2

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
 print("Hello, World!")
+a=1
+b=3
+c=a-b
+print(c)
+print(a + b)
+z=9
+print(z%b)
+print(type(c))
+d=int(input("Enter a number d:"))
+e=int(input("enter the number e:"))
+# f=int(d) + int(e)
+print("avg of d and e:", (d+e) /2)
+print(square:=d**2)
+print(type(square))
