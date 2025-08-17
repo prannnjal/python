2f9acd0535ac80f96cb859a860dde1c9845eb4aa
prannnjal_ <pranjalkumar445@gmail.com>
2025-08-18 00:12:17 +0530

    prblm 5

--- list.py
+++ list.py
@@ -4,15 +4,28 @@
 # print(list[1])
 # list.append("kiwi")  # Adding "kiwi" to the end of the list
 # print(list)
-para=[111, 2, 3, 85, 90, 9, 45, 90, 100]
-print(para)  # Accessing the fourth element of the list
+# para=[111, 2, 3, 85, 90, 9, 45, 90, 100]
+# print(para)  # Accessing the fourth element of the list
 # para.sort()
 # print(para)
 # para.reverse()
 # print(para)
 # para.pop(0)  # Removing the element at index 2
-para.remove(90)
-print(para)
-print(type(para))  # Checking the type of the list
+# para.remove(90)
+# print(para)
+# print(type(para))  # Checking the type of the list
+# practice
+b = []
+b1 = int(input("Enter a number: "))
+b.append(b1)
+b2 = int(input("Enter another number: "))
+b.append(b2)
+b3 = int(input("Enter one more number: "))
+b.append(b3)
+b4 = int(input("Enter a final number: "))   
+b.append(b4)
 
-
+print(b)  # Printing the list of numbers
+b.sort()  # Sorting the list of numbers
+print(b)  # Printing the sorted list of numbers
+print(sum(b))  # Printing the sum of the numbers in the list
--- tupple.py
+++ tupple.py
@@ -1,6 +1,19 @@
-a=(1,67, "idi", "pranjal","idi" )
-print(type(a))  # Checking the type of the tuple
-no = a.count("idi")
-print(no)  # Printing the count of "idi" in the tuple
-i = a.index("pranjal")
-print(i)  # Printing the index of the first occurrence of "idi"
+# a=(1,67,  )
+# print(type(a))  # Checking the type of the tuple
+# no = a.count("idi")
+# print(no)  # Printing the count of "idi" in the tuple
+# i = a.index("pranjal")
+# print(i)  # Printing the index of the first occurrence of "idi"
+# b=(2,3,4,5  )
+# concatenated_tuple = a + b  # Concatenating two tuples
+# print(concatenated_tuple)  # Printing the concatenated tuple
+# sorted_tuple = tuple(sorted(concatenated_tuple))  # Sorting the concatenated tuple
+# print(sorted_tuple)  # Printing the sorted tuple
+# print(5 in sorted_tuple)  # Checking if 5 is in the sorted tuple
+# print(10 in sorted_tuple)  # Checking if 10 is in the sorted tuple
+# print(len(sorted_tuple))  # Printing the length of the sorted tuple
+# a,b,c = 1, 2, 3  # Unpacking values into variables
+# print(a, b, c)  # Printing the unpacked values
+a=(1, 0, 3, 0, 6, 0)
+no = a.count(0)
+print(no)  # Printing the count of 0 in the tuple
