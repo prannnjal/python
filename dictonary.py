31275d24b0ef147b6e99770765cc0926b4f0e0da
prannnjal_ <pranjalkumar445@gmail.com>
2025-08-20 17:44:00 +0530

    Track migrations properly and update .gi

--- dictonary.py
+++ dictonary.py
@@ -1,16 +1,33 @@
-a={"name": "John", "age": 30, "city": "New York"}
-print(a["age"])  # Printing the dictionary
-print(a.items())  # Printing the items in the dictionary
-print(a.keys())  # Printing the keys in the dictionary
-a.update({"age": 31, "height": 175})  # Updating the age and adding height to the dictionary
-print(a["age"])  # Printing the updated age
-print(a)  # Printing the added height
-print(a.get("namee"))  # Using get to retrieve the name
-b=dict.copy(a)  # Copying the dictionary a to b
-print(b)  # Printing the copied dictionary
+# a={"name": "John", "age": 30, "city": "New York"}
+# print(a["age"])  # Printing the dictionary
+# print(a.items())  # Printing the items in the dictionary
+# print(a.keys())  # Printing the keys in the dictionary
+# a.update({"age": 31, "height": 175})  # Updating the age and adding height to the dictionary
+# print(a["age"])  # Printing the updated age
+# print(a)  # Printing the added height
+# print(a.get("namee"))  # Using get to retrieve the name
+# b=dict.copy(a)  # Copying the dictionary a to b
+# print(b)  # Printing the copied dictionary
 # a.clear()  # Clearing the dictionary a
 # print(a)  # Printing the cleared dictionary
-a.pop("city")  # Removing the city from the dictionary
-print(a)  # Printing the dictionary after removing city
-a.popitem()  # Removing the last inserted item from the dictionary
-print(a)  # Printing the dictionary after popping the last item
+# a.pop("city")  # Removing the city from the dictionary
+# print(a)  # Printing the dictionary after removing city
+# a.popitem()  # Removing the last inserted item from the dictionary
+# print(len(a))  # Printing the dictionary after popping the last item
+
+# Practice
+# a={"naam": "name", "umar": "age","kaam": "work"}
+# c=input("Enter the key to get the value: ")
+# print(a.get(c, "Key not fo
+# 
+# und"))  # Using get to retrieve the value
+
+a = {}
+pairs = input("Enter the key-value pairs (key:value) separated by space: ").split(" ")
+for pair in pairs:
+    if ":" in pair:
+        key, value = pair.split(":", 1)
+        a[key] = value
+print(a)  # Printing the dictionary with the key-value pairs
+c = input("Enter the key to get the value: ")
+print(a.get(c, "Key not found"))  # Using get to retrieve the value for the given key
---
+++ sets.py
@@ -0,0 +1,29 @@
+# a={1,2,3,7}
+# print(type(a))  # Printing the type of a, which is a set
+# print(len(a))  # Printing the length of the set
+# a.update([4, 5, 6])  # Adding an element to the set
+# print(a)  # Printing the set after adding an element
+# b={7, 8, 9, 1}
+# print(b)  # Printing the new set
+# print(a.union(b))  # Printing the union of two sets
+# print(a.intersection(b))  # Printing the intersection of two sets
+# c=a.intersection(b)  # Finding the intersection of two sets
+# print(c)  # Printing the intersection set
+# d=a.union(b)  # Finding the union of two sets
+# print(d)  # Printing the union set
+# print(c.issubset(a))  # Checking if set c is a subset of set a
+
+# practice
+# a=set(input("Enter the elements of the set separated by space: ").split())
+# b=set(input("Enter another set of elements separated by space: ").split())
+
+# print("Union of sets:", a.union(b))  # Printing the union of two sets
+
+# s =set()
+# s.add(20)
+s.add(20.0)
+# s.add('20')
+# print(len(s))  # Printing the length of the set
+# print(s)  # Printing the set
+# print(type(s))  # Printing the type of the set
+
